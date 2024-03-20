064a380ee2376063ee8d8c496dc69abc184afd60
sukh <sukhsinghkainth@gmail.com>
2024-03-20 18:07:23 +0530

    handle token expiry time with jwt-decode

--- src/app/services/interceptor/custom.interceptor.ts
+++ src/app/services/interceptor/custom.interceptor.ts
@@ -6,13 +6,23 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { jwtDecode } from 'jwt-decode';
+import { AuthServiceService } from '../authService/auth-service.service';
 
 
 @Injectable()
 export class CustomInterceptor implements HttpInterceptor {
   private data = localStorage.getItem('token');
-  constructor() { }
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  constructor(private auth: AuthServiceService) { }
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.data !== null) {
+      const decodedToken: any = jwtDecode(this.data);
+      const currentTime = Date.now() / 1000;
+      if (decodedToken.exp < currentTime) {
+        this.auth.loggedOut();
+        throw new Error("Not authenticated out")
+      }
+    }
     const cloneRequest = request.clone({
       setHeaders: {
         Authorization: `Bearer ${this.data}`
